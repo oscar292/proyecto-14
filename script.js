88a96eb218d1e716474911771961b13216633f6a
oscar292 <oseduar123@gmail.com>
2025-02-13 15:38:33 -0500

    function

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const flipBook = (elBook) => {
+  console.log("Initializing book...");
   elBook.style.setProperty("--c", 0); // Set current page
   elBook.querySelectorAll(".page").forEach((page, idx) => {
     page.style.setProperty("--i", idx);
     page.addEventListener("click", (evt) => {
       if (evt.target.closest("a")) return;
       const curr = evt.target.closest(".back") ? idx : idx + 1;
+      console.log("Page clicked, current page:", curr);
       elBook.style.setProperty("--c", curr);
     });
   });
